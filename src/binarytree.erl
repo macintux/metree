1d88a654d5f68645ba77df7ff74cabfbb3ae2770
John Daily <git@epep.us>
2019-11-29 13:45:29 -0500

    Expand simple binary tree

--- src/binarytree.erl
+++ src/binarytree.erl
@@ -1,9 +1,14 @@
 %% Binary tree.
+%%
+%% The intent for this module is to explore the possibilities for
+%% building a binary search tree using this code, by choosing another
+%% type of binary tree and doing the same to identify commonalities.
 -module(binarytree).
 -export([
          root/1,
          insert/3,
-         retrieve/2
+         retrieve_path/2,
+         retrieve_all/1
         ]).
 
 root(Val) ->
@@ -24,9 +29,14 @@ insert(NewVal, [left|PathTail], {Val, Left, Right}) ->
 insert(NewVal, [right|PathTail], {Val, Left, Right}) ->
     {Val, Left, insert(NewVal, PathTail, Right)}.
 
-retrieve([], {Val, _Left, _Right}) ->
+retrieve_path([], {Val, _Left, _Right}) ->
     Val;
-retrieve([left|Tail], {_Val, Left, _Right}) ->
-    retrieve(Tail, Left);
-retrieve([right|Tail], {_Val, _Left, Right}) ->
-    retrieve(Tail, Right).
+retrieve_path([left|Tail], {_Val, Left, _Right}) ->
+    retrieve_path(Tail, Left);
+retrieve_path([right|Tail], {_Val, _Left, Right}) ->
+    retrieve_path(Tail, Right).
+
+retrieve_all(empty) ->
+    [];
+retrieve_all({Val, Left, Right}) ->
+    [Val] ++ retrieve_all(Left) ++ retrieve_all(Right).
--- test/binarytree_tests.erl
+++ test/binarytree_tests.erl
@@ -4,10 +4,19 @@
 insert_test() ->
     Root = binarytree:root(5),
     Root2 = binarytree:insert(4, [left], Root),
-    4 = binarytree:retrieve([left], Root2).
+    4 = binarytree:retrieve_path([left], Root2).
 
 insert_inline_test() ->
     Root = binarytree:root(5),
     Root2 = binarytree:insert(4, [left], Root),
     Root3 = binarytree:insert(3, [left], Root2),
-    4 = binarytree:retrieve([left, left], Root3).
+    4 = binarytree:retrieve_path([left, left], Root3).
+
+depth_first_retrieve_test() ->
+    Root = binarytree:root(5),
+    Root2 = binarytree:insert(4, [left], Root),
+    Root3 = binarytree:insert(3, [left], Root2),
+    Root4 = binarytree:insert(6, [right], Root3),
+    Root5 = binarytree:insert(2, [right, left], Root4),
+    Root6 = binarytree:insert(1, [right, right], Root5),
+    [5, 3, 4, 6, 2, 1] = binarytree:retrieve_all(Root6).
