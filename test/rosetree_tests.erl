1e3b8bedd07d7f39f85d1370f615b7551dadfa36
John Daily <git@epep.us>
2019-12-22 21:07:04 -0500

    Test distance

--- test/rosetree_tests.erl
+++ test/rosetree_tests.erl
@@ -8,3 +8,13 @@ insert_test() ->
     Root3 = rosetree:insert(rosetree:new_node("C"),
                             "B", Root2),
     ["A", "B"] = rosetree:find("C", Root3).
+
+distance_test() ->
+    Root = rosetree:root("A"),
+    Root2 = rosetree:insert(rosetree:new_node("B"),
+                            "A", Root),
+    Root3 = rosetree:insert(rosetree:new_node("C"),
+                            "B", Root2),
+    2 = rosetree:distance("A", "C", Root3),
+    2 = rosetree:distance("C", "A", Root3),
+    0 = rosetree:distance("B", "B", Root3).
